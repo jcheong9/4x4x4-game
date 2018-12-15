0b32c5617ad8080ba3e1c17056803bf81ac45c86
Smallyellowface <jctheathlete@gmail.com>
2018-12-14 21:17:51 -0800

    added player two mode

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-const PLAYER_TOKEN = 'X';
+const PLAYERONE_TOKEN = 'X';
+const PLAYERTWO_TOKEN = 'O';
 
+let playerTurn = 0;
 $(document).ready(function(){
   const grid = [
     [' ', ' ', ' ', ' '],
@@ -10,14 +12,25 @@ $(document).ready(function(){
 
   $('.col').click(function(){
     $this = $(this);
-    $this.html(PLAYER_TOKEN);
-    const i = $this.data('i');
-    const j = $this.data('j');
-    grid[i][j] = PLAYER_TOKEN;
-
-    if (isGameOver()){
-      console.log('winner');
-      alert('You Win');
+    if (playerTurn == 0) {
+      $this.html(PLAYERONE_TOKEN);
+      const i = $this.data('i');
+      const j = $this.data('j');
+      grid[i][j] = PLAYERONE_TOKEN;
+      playerTurn = 1;
+    } else if (playerTurn == 1) {
+      $this.html(PLAYERTWO_TOKEN);
+      const i = $this.data('i');
+      const j = $this.data('j');
+      grid[i][j] = PLAYERTWO_TOKEN;
+      playerTurn = 0;
+    }
+     if (isGameOver() === PLAYERONE_TOKEN){
+      console.log('Player one Wins');
+      alert('Player One Wins');
+    } else if (isGameOver() === PLAYERTWO_TOKEN) {
+      console.log('Player Two Wins');
+      alert('Player Two Wins');
     }
   });
 
@@ -28,16 +41,16 @@ $(document).ready(function(){
 
   function isGameOver(){
     //only check rows
-  for(let i = 0; i < 4; i++){
-    if(grid[i][0] !== ' ' && grid[i][0] === grid[i][1] && grid[i][0] === grid[i][2] && grid[i][0] === grid[i][3]) {
-      return grid[i][0];
+    for(let i = 0; i < 4; i++){
+      if(grid[i][0] !== ' ' && grid[i][0] === grid[i][1] && grid[i][0] === grid[i][2] && grid[i][0] === grid[i][3]) {
+        return grid[i][0];
+      }
     }
-  }
-  //only check vertical
-  for(let j = 0; j < 4; j++){
-    if(grid[0][j] !== ' ' && grid[0][j] === grid[1][j] && grid[0][j] === grid[2][j] && grid[0][j] === grid[3][j]) {
-      return grid[0][j];
-  }
+    //only check vertical
+    for(let j = 0; j < 4; j++){
+      if(grid[0][j] !== ' ' && grid[0][j] === grid[1][j] && grid[0][j] === grid[2][j] && grid[0][j] === grid[3][j]) {
+        return grid[0][j];
+    }
 }
 
 
